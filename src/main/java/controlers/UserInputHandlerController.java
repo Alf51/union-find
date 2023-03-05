90ef612fab5d931bcd6f50e4f64a1ea449b02d59
Vasiliy <zakupm51@gmail.com>
2023-03-05 17:54:19 +0300

    Added class to handle user input: UserInputHandlerController

---
+++ src/main/java/controlers/UserInputHandlerController.java
@@ -0,0 +1,39 @@
+package controlers;
+
+import model.sortingTools.UnionFindGroup;
+import view.Display;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class UserInputHandlerController {
+    public static void parseUserArgument(String[] userArguments) {
+        List<String> userInputArgument = new ArrayList<>(Arrays.asList(userArguments));
+
+        if (userInputArgument.size() < 1) {
+            System.out.println("The required file name argument is missing");
+            System.exit(1);
+        }
+
+        String outPutFile = userInputArgument.get(0);
+
+        File file = new File(outPutFile);
+        if (!file.canRead()) {
+            System.out.println("Unable to read the file");
+            System.exit(1);
+        }
+
+        try {
+            Set<String> setString = UnionFindGroup.getCorrectSetStringLine(file);
+            List<List<String>> resultGroup = UnionFindGroup.getGroupsList(setString);
+            Display.printListGroup(resultGroup);
+        } catch (RuntimeException e) {
+            System.out.println(e.getLocalizedMessage());
+            System.out.println("Error during sorting");
+            System.exit(1);
+        }
+    }
+}
---
+++ src/main/java/view/Display.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.util.List;
+
+public class Display {
+    public static void printListGroup(List<List<String>> listGroup) {
+        System.out.println("the number of groups with more than one element received: " + listGroup.size());
+        System.out.println();
+        for (int i = 0; i < listGroup.size(); i++) {
+            List<String> groupss = listGroup.get(i);
+            if (groupss.size() != 1) {
+                System.out.println("Group number " + (i + 1));
+                for (String s : groupss) {
+                    System.out.println(s);
+                }
+                System.out.println();
+            }
+        }
+        System.out.println("the number of groups with more than one element received: " + listGroup.size());
+    }
+}
